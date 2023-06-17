d1b8f7f560e77913e4b0cee54b617bbd75e8672d
mahagarmr <mahagarmr@gmail.com>
2023-06-17 13:30:54 +0300

    Добавьте файлы проекта.

---
+++ hw-winforms-calendar/Form1.Designer.cs
@@ -0,0 +1,57 @@
+﻿namespace hw_winforms_calendar
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dateTimePicker1 = new DateTimePicker();
+            SuspendLayout();
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.Location = new Point(87, 12);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(200, 23);
+            dateTimePicker1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.InactiveCaption;
+            ClientSize = new Size(377, 205);
+            Controls.Add(dateTimePicker1);
+            Name = "Form1";
+            Text = "calendar";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DateTimePicker dateTimePicker1;
+    }
+}
---
+++ hw-winforms-calendar/Form1.cs
@@ -0,0 +1,10 @@
+namespace hw_winforms_calendar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
